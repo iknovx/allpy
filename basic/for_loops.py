3cddeace122019be3b6f6de7bcc7f499af322c98
Dzmitry <dinqfh@gmail.com>
2026-06-20 13:16:22 +0200

    for loops

---
+++ basic/for_loops.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    print(f'{i+1} = {i}')
+    
+for i in range(1, 6):
+    print(f'{i} = {i-1}')
+
+for i in range(0, 10, 2):
+    print(f'{i} = {i//2}')
+res = 1
+for i in range(10):
+    res += i
+    i += res
+    print(i)
