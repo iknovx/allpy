d3c69f9ca8183866871960c9f6544efa508562ce
lmkptr <dinqfh@gmail.com>
2026-06-23 03:57:16 +0200

    new updates

--- basics_py/projects/simple_timer.py
+++ basics_py/projects/simple_timer.py
@@ -1,9 +1,14 @@
 import time 
+def timer():
+    x = int(input("Enter a number to countdown from: "))
 
-x = int(input("Enter a number to countdown from: "))
-
-for i in range(x, 0-1, -1):
-    print(f'{i}s')
-    time.sleep(1)
-with open("basic/projects/logs/countdown.txt", "w") as f:
-    f.write(f'Countdown from {x} completed.')
+    for i in range(x, 0-1, -1):
+       time.sleep(1)
+       print(f'{i}s')
+    
+    time.sleep(.5)
+    print("time is over")
+    
+    
+    with open("basics_py/projects/logs/countdown.txt", "w") as f:
+        f.write(f'Countdown from {x} completed.')
--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from basics_py.projects.calculator import calculator
+from basics_py.projects.simple_timer import timer
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    calculator()
+    print("Hello and welcome to menu")
+    while True:
+        print("Press 1 if you want to calculate something and 2 if you need timer: ")
+        user_choice = int(input("Choose one of them: "))
+
+        match user_choice:
+            case 1:
+                calculator()
+            case 2:
+                timer()
+            case 3:
+                print("The end")
+                break
+            case _:
+                print("Invalid choice")
+            
